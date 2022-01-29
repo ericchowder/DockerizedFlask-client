5361403abd1057ad614b6790e62a9b2d3a5eb19e
ericchowder <linksta7@gmail.com>
2022-01-29 18:47:03 -0500

    new webpack plugin definition

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,11 @@ module.exports = {
   ],
   configureWebpack: {
     plugins: [
-      new Dotenv()
+      new Dotenv(),
+      new webpack.DefinePlugin({           
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),      
+        API_HOST: JSON.stringify(process.env.API_HOST)
+      })
     ]
   }
   // ,
